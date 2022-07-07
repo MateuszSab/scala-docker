cc60830b210f86fcc65357f0347d8c4f462a84de
MateuszSab <ma.sabatowski@gmail.com>
2022-07-07 13:23:02 +0200

    first question answered

--- README.md
+++ README.md
@@ -1,9 +1,6 @@
 # scala-docker
 
-`sbt docker` - buiild docker image
+`sbt docker` - build docker image
 
 `docker run -v /c:/mateusz/ mateusz/scala-docker:0.1.0-SNAPSHOT /mateusz/{path to your harvest file} /mateusz/{path to your prices file}` - run docker container 
 
-To provide an actual solution I guess I should turn some columns into lists, and use list functions to transform them and perform computations. 
-
-To little time to figure it out right now.
--- src/main/scala/Application.scala
+++ src/main/scala/Application.scala
@@ -1,37 +1,89 @@
 object Application extends App {
 
-  if (args.length < 1) {
-    println("ERROR: lack of argument")
-    sys.exit(-1)
-  }
-  val path = args(0)
+  // DATA
+  val sample = io.Source.fromFile("C:\\scala-projects\\scala-docker\\src\\main\\resources\\harvest.csv")
 
-  if (args.length < 2) {
-    println("ERROR: lack of argument")
-    sys.exit(-1)
-  }
+  val rows = sample.getLines.foldLeft(Seq.empty[Seq[String]]) { (result, line) =>
+    result :+ line.split(",").toSeq
+  }.tail
 
-  val path1 = args(1)
 
-  println(s"loading files from: $path")
-  println(s"loading files from: $path1")
+  // METHODS
 
-  val harvest = io.Source.fromFile(path)
-  for (line <- harvest.getLines) {
-    val cols = line.split(",").map(_.trim)
-    println(s"${cols(0)}|${cols(1)}|${cols(2)}|${cols(3)}")
-
+  def selectName(name: String, data: Seq[Seq[String]]): Seq[Seq[String]] = {
+    data.foldLeft(Seq.empty[Seq[String]]) { (result, seq) =>
+      if (seq.head == name) result :+ seq
+      else result
     }
-    harvest.close
-
-  val prices = io.Source.fromFile(path1)
-  for (line <- prices.getLines) {
-    val cols = line.split(",").map(_.trim)
-        println(s"${cols(0)}|${cols(1)}|${cols(2)}")
   }
-  prices.close
 
+  def maxAmount(data: Seq[Seq[String]]): Seq[String] = {
+    data.foldLeft(data.head) { (result, seq) =>
+      if (seq.last.toDouble > result.last.toDouble) seq
+      else result
+    }
   }
+
+  def selectMonth(monthNumber: String): Seq[Seq[String]] = {
+    rows.foldLeft(Seq.empty[Seq[String]]) { (result, seq) =>
+      if (seq(1).contains(monthNumber)) result :+ seq
+      else result
+    }
+  }
+
+  def sumAmounts(data: Seq[Seq[String]]): Double = {
+    data.foldLeft(data.head.last.toDouble) { (result, seq) =>
+      result + seq.last.toDouble
+    }
+  }
+
+  def bestInMonth(month: String): Seq[String] = {
+    val selectedMon = selectMonth(month)
+    val namesAndAmounts = listOfNames.map(name => Seq(name, sumAmounts(selectName(name, selectedMon)).toString))
+    maxAmount(namesAndAmounts)
+  }
+
+  def bestByFruit(fruit: String): Seq[String] = {
+    val selectedFruit = selectFruits(fruit)
+    val namesAndAmountsForFruits = listOfNames.map(name => Seq(name, sumAmounts(selectName(name, selectedFruit)).toString))
+    maxAmount(namesAndAmountsForFruits)
+  }
+
+
+  def selectFruits(fruit: String): Seq[Seq[String]] = {
+    rows.foldLeft(Seq.empty[Seq[String]]) { (result, seq) =>
+      if (seq(2).contains(fruit)) result :+ seq
+      else result
+    }
+  }
+
+  // VALUES
+  val listOfMonths = rows.foldLeft(Seq.empty[String]) { (result, seq) =>
+    result :+ seq(1).take(7)
+  }.distinct
+
+  val listOfNames = rows.foldLeft(Seq.empty[String]) { (result, seq) =>
+    result :+ seq.head
+  }.distinct
+
+  val listOfFruits = rows.foldLeft(Seq.empty[String]) { (result, seq) =>
+    result :+ seq(2)
+  }.distinct
+
+
+  // BEST IN A MONTH
+//  println(listOfMonths, listOfFruits, listOfNames)
+
+  val bestIEachMonth = listOfMonths.map(month => bestInMonth(month) :+ month.mkString)
+  println(bestIEachMonth)
+
+  val bestByEachFruit = listOfFruits.map(fruit => bestByFruit(fruit) :+ fruit.mkString)
+  println(bestByEachFruit)
+
+
+  sample.close
+
+}
 
 
 
